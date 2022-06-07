703eb4f4f0ef66d89be6782e7371a2d72b8aa97a
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2022-06-07 17:19:55 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [news, setNews] = useState([]);
+
+  const fetchNews = () => {
+    
+  }
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
