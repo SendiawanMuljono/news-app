70188fc633d488c53f5453977bcb6208c818c2b1
Sendiawan Muljono <sendiawanmuljono@gmail.com>
2022-06-17 16:59:34 +0700

    Add Loading Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import LoadingBar from './LoadingBar.svg'
 
 function App() {
   const [news, setNews] = useState([]);
-  const [searchQuery, setSearchQuery] = useState("react");
+  const [searchQuery, setSearchQuery] = useState("");
   const [url, setUrl] = useState("https://hn.algolia.com/api/v1/search?query=react");
   const [loading, setLoading] = useState(false);
 
@@ -36,14 +37,39 @@ function App() {
 
   return (
     <div className="App container">
-      <h1>News</h1>
+      <h1 className='text-center mt-3 mb-3'>News</h1>
       <form onSubmit={handleSubmit}>
         <div className="input-group mb-3">
           <input type="text" className="form-control" onChange={handleChange}/>
           <button className="btn btn-outline-secondary">Search</button>
         </div>
       </form>
-      {loading === true ? (<h5>LOADING...</h5>) : news.map((n, i) => (<p key={i}>{n.title}</p>))}
+      {loading === true ? (
+        <div className='d-flex justify-content-center'>
+          <img src={LoadingBar} alt=''></img>
+        </div>
+      ) : news.map((n, i) => (
+        <div>
+          <div className='border border-success border-1 rounded-3 mb-2 p-2'>
+            <div className='d-flex'>
+              <div className='fw-bold' key={i}>Title:</div>&nbsp;
+              <div>{n.title}</div>
+            </div>
+            <div className='d-flex'>
+              <div className='fw-bold' key={i}>URL:</div>&nbsp;
+              <a href={n.url}>{n.url}</a>
+            </div>
+            <div className='d-flex'>
+              <div className='fw-bold' key={i}>Author:</div>&nbsp;
+              <div>{n.author}</div>
+            </div>
+            <div className='d-flex'>
+              <div className='fw-bold text-danger' key={i}>Points:</div>&nbsp;
+              <div className='text-danger'>{n.points}</div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
